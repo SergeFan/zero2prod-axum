393a4ae8bec09b513844e7d4e51940ff2302706c
Jaeson Fan <sergefan@live.com>
2024-04-21 00:55:34 +0900

    Try to figure out how to spawn app in axum

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use axum::routing::get;
-use axum::Router;
+use zero2prod_axum::{app, listener};
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(root));
+async fn main() -> Result<(), std::io::Error> {
+    let app = app();
+    let listener = listener().await;
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> &'static str {
-    "Hello, world!"
+    axum::serve(listener, app).await
 }
